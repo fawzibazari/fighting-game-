a7587bac481c6b5532e6bcd7019bc067500a5959
fawzibazari <fawzi.bazari2@gmail.com>
2022-04-14 16:45:46 +0200

    working on it

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
-const canvas = document.querySelector('canvas')!
-const c = canvas.getContext('2d')!
+const canvas = document.querySelector('canvas')!;
+const c = canvas.getContext('2d')!;
 
-canvas.width = 1420
-canvas.height = 780
+canvas.width = 1420;
+canvas.height = 780;
 
-c.fillRect(0, 0, canvas.width, canvas.height)
+c.fillRect(0, 0, canvas.width, canvas.height);
 
-const gravity = 0.7
- 
+const gravity = 0.7;
+
+interface Characteristics {
+  position: string;
+  velocity: string;
+  width: string;
+  height: string;
+  lastKey: string;
+  attackBox: JSON;
+  color: string;
+  isAttacking: string;
+  health: string;
+}
